109f12f28c04f1f04e8653c53adbfbd9d68c4f29
Saba Sakandelidze <101635263+sabasako@users.noreply.github.com>
2022-09-10 00:42:00 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -16,60 +16,102 @@ const specialAc = document.querySelector(".special-ac");
 const specialDel = document.querySelector(".special-del");
 const specialEqual = document.querySelector(".special-equal");
 
-lightBtn.forEach((item) => {
-  item.addEventListener("click", () => {
-    darkDiv.classList.add("hidden");
-    lightDiv.classList.remove("hidden");
-    body.style.background = "#daf0ff";
-    calculator.style.background = "#f7f8fb";
-    buttons.forEach((item) => {
-      item.style.background =
-        "linear-gradient(129.7deg, rgba(255, 255, 255, 0.6) -9.69%, rgba(255, 255, 255, 0.4) 114.23%)";
-      item.style.border = "1px solid #ffffff";
-    });
-    modsDiv.style.backgroundColor = "hsl(0, 0%, 93%)";
-    equal.style.boxShadow =
-      "-9px 13px 23px rgba(0, 163, 255, 0.65), inset -3px 4px 11px #b0dfff;";
-    calculatorShade.style.cssText =
-      "background: rgba(96, 190, 255, 0.55); filter: blur(123px);";
-    specialBtn.forEach((item) => {
-      item.style.backgroundColor = "#ade2ff";
-    });
-    specialAc.style.cssText = "background: #fff;";
-    specialDel.style.cssText = "border: 1px solid #ffffff;";
-    specials.forEach((item) => {
-      item.style.cssText =
-        "filter: brightness(0) saturate(100%) invert(56%) sepia(0%) saturate(0%) hue-rotate(112deg) brightness(94%) contrast(89%)";
-    });
-    specialEqual.style.cssText =
-      "background: #19acff; box-shadow: -9px 13px 23px rgba(0, 163, 255, 0.65), inset -3px 4px 11px #b0dfff; border: none;";
+//////////////////////////////
+//        FUNCTIONS         //
+//////////////////////////////
+
+function lightMode() {
+  darkDiv.classList.add("hidden");
+  lightDiv.classList.remove("hidden");
+  specialAc.classList.add("hover-gray");
+  specialDel.classList.add("hover-gray");
+  specialEqual.classList.add("hover-equal");
+  specialAc.classList.remove("dark-hover-gray");
+  specialDel.classList.remove("dark-hover-gray");
+  specialEqual.classList.remove("dark-hover-equal");
+  body.style.backgroundColor = "#daf0ff";
+  calculator.style.background = "#f7f8fb";
+  modsDiv.style.backgroundColor = "hsl(0, 0%, 93%)";
+  // calculatorShade.style.cssText =
+  //   "background-color: rgba(96, 190, 255, 0.55); filter: blur(123px);";
+  calculatorShade.style.backgroundColor = "rgba(96, 190, 255, 0.55)";
+  calculatorShade.style.filter = "blur(123px)";
+
+  buttons.forEach((item) => {
+    item.style.backgroundColor = "#fffffffc";
+    item.style.border = "1px solid #ffffff";
+    item.classList.add("hover");
+    item.classList.remove("dark-hover");
   });
+  specialBtn.forEach((item) => {
+    item.style.backgroundColor = "#ade2ff";
+  });
+  specialAc.style.backgroundColor = "#fff";
+  // specialDel.style.cssText =
+  //   "border: 1px solid #fff; background-color: #fffffffc;";
+  specialDel.style.border = "1px solid #fff";
+  specialDel.style.backgroundColor = "#fffffffc";
+  specials.forEach((item) => {
+    // item.style.cssText =
+    //   "filter: brightness(0) saturate(100%) invert(56%) sepia(0%) saturate(0%) hue-rotate(112deg) brightness(94%) contrast(89%)";
+    item.style.filter =
+      "brightness(0) saturate(100%) invert(56%) sepia(0%) saturate(0%) hue-rotate(112deg) brightness(94%) contrast(89%)";
+  });
+  // specialEqual.style.cssText = "background-color: #19acff;";
+  specialEqual.style.backgroundColor = "#19acff";
+}
+
+function darkMode() {
+  darkDiv.classList.remove("hidden");
+  lightDiv.classList.add("hidden");
+  specialAc.classList.remove("hover-gray");
+  specialDel.classList.remove("hover-gray");
+  specialEqual.classList.remove("hover-equal");
+  specialAc.classList.add("dark-hover-gray");
+  specialDel.classList.add("dark-hover-gray");
+  specialEqual.classList.add("dark-hover-equal");
+  body.style.backgroundColor = "#343f46";
+  calculator.style.backgroundColor = "#17181A";
+  modsDiv.style.backgroundColor = "#303136";
+  equal.style.boxShadow = "none";
+  // calculatorShade.style.cssText =
+  //   "background-color: rgba(0, 107, 181, 0.63); filter: blur(140px);";
+  calculatorShade.style.backgroundColor = "rgba(0, 107, 181, 0.63)";
+  calculatorShade.style.filter = "blur(140px)";
+  buttons.forEach((item) => {
+    // item.style.cssText = "background-color: #303136;";
+    item.style.backgroundColor = "#303136";
+    item.style.border = "none";
+    item.classList.remove("hover");
+    item.classList.add("dark-hover");
+  });
+  specialBtn.forEach((item) => {
+    item.style.backgroundColor = "#005DB2";
+  });
+  // specialAc.style.cssText = "background-color: #616161; border: none;";
+  specialAc.style.backgroundColor = "#616161";
+  specialAc.style.border = "none";
+  specialDel.style.backgroundColor = "#616161";
+  specialDel.style.border = "none";
+  // specialDel.style.cssText = "background-color: #616161; border: none;";
+  specials.forEach((item) => {
+    // item.style.cssText =
+    //   "filter: brightness(0) saturate(100%) invert(85%) sepia(0%) saturate(317%) hue-rotate(145deg) brightness(80%) contrast(84%)";
+    item.style.filter =
+      "brightness(0) saturate(100%) invert(85%) sepia(0%) saturate(317%) hue-rotate(145deg) brightness(80%) contrast(84%)";
+  });
+  specialEqual.style.backgroundColor = "#1991FF";
+  // specialEqual.style.cssText = "background-color: #1991FF;";
+}
+
+//////////////////////////////
+//      EventListeners      //
+//////////////////////////////
+
+lightBtn.forEach((item) => {
+  item.addEventListener("click", lightMode);
 });
 
 darkBtn.forEach((item) => {
-  item.addEventListener("click", () => {
-    darkDiv.classList.remove("hidden");
-    lightDiv.classList.add("hidden");
-    body.style.background = "#343f46";
-    calculator.style.background =
-      "linear-gradient(191.34deg, #17181A -4.95%, #17181A 103.74%)";
-    buttons.forEach((item) => {
-      item.style.background = "#303136";
-      item.style.border = "none";
-    });
-    modsDiv.style.backgroundColor = "#303136";
-    equal.style.boxShadow = "none";
-    calculatorShade.style.cssText =
-      "background: rgba(0, 107, 181, 0.63); filter: blur(140px);";
-    specialBtn.forEach((item) => {
-      item.style.backgroundColor = "#005DB2";
-    });
-    specialAc.style.cssText = "background: #616161; border: none;";
-    specialDel.style.cssText = "background: #616161; border: none;";
-    specials.forEach((item) => {
-      item.style.cssText =
-        "filter: brightness(0) saturate(100%) invert(85%) sepia(0%) saturate(317%) hue-rotate(145deg) brightness(80%) contrast(84%)";
-    });
-    specialEqual.style.background = "#1991FF";
-  });
+  item.addEventListener("click", darkMode);
 });
